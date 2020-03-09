0ad451d104cb495ad7daffd039293029fe44d438
AqSlz <44170778+AqSlz@users.noreply.github.com>
2020-03-09 16:47:26 -0400

    sidebar remodel

--- client/src/views/Services/SearchBar.js
+++ client/src/views/Services/SearchBar.js
@@ -6,22 +6,9 @@ import TextField from "@material-ui/core/TextField";
 const SearchBar = props => {
     return (
         <div className="SearchBar">
-            <TextField
-                id="search"
-                label="Search services here"
-                variant="outlined"
-                autoComplete="off"
-                style={{ width: 412.69, marginLeft: 50 }}
-                value={props.filterText}
-                onChange={event => {
-                    props.setFilterText(event.target.value);
-                }}
-            />
             <span className="spacer" />
             <div className="buttonHolder">
-                <Button key={"Book an appointment"} color="primary" variant="outlined">
-                    Book an appointment
-                </Button>
+                
             </div>
             <div className="buttonHolder">
                 {/* IconButton */}
--- client/src/views/Services/SideBar.js
+++ client/src/views/Services/SideBar.js
@@ -1,70 +1,126 @@
 import React from "react";
-import { Button } from "@material-ui/core";
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import './Services.css';
 
-const SideBar = (props) => {
+import { Button, makeStyles } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+
+import "./Services.css";
+
+const useStyles = makeStyles(theme => ({
+    options: {
+        alignItems: "flex-start",
+        display: "contents",
+        marginLeft: 5,
+        paddingBottom: 5,
+    },
+    title: {
+        marginLeft: 5,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+}));
+
+const SideBar = props => {
+    const classes = useStyles();
     return (
-        <div className="SideBar">
-            
-            <div className="titleBox">
-                <FormControlLabel
-                    className="SideBarTitle"
-                    value="???"
-                    control={<Button className="checkBoxes" size="small">Clear</Button>}
-                    label="Filter by:"
-                    labelPlacement="start"
-                />
+        <div>
+            <div className="SideBar">
+                <div className="titleBox">
+                    <FormControlLabel
+                        className="SideBarTitle"
+                        value="???"
+                        control={
+                            <Button className="checkBoxes" size="small">
+                                Clear
+                            </Button>
+                        }
+                        label="Filter by:"
+                        labelPlacement="start"
+                        classes={{ root: classes.title }}
+                    />
+                </div>
+
+                <div>
+                    <TextField
+                        id="search"
+                        label="Search services here"
+                        variant="filled"
+                        autoComplete="off"
+                        value={props.filterText}
+                        /*onChange={event => {
+                            props.setFilterText(event.target.value);
+                        }}*/
+                        style={{ width: "100%" }}
+                    />
+                </div>
+
+                <div className="itemBox">
+                    <FormControlLabel
+                        className="checkBoxName"
+                        value="???"
+                        classes={{ root: classes.options }}
+                        control={
+                            <Checkbox
+                                color="secondary"
+                                className="checkBoxes"
+                            />
+                        }
+                        label="Hair Care"
+                    />
+                </div>
+
+                <div className="itemBox">
+                    <FormControlLabel
+                        className="checkBoxName"
+                        value="???"
+                        control={<Checkbox color="secondary" />}
+                        label="Haircuts"
+                        classes={{ root: classes.options }}
+                    />
+                </div>
+
+                <div className="itemBox">
+                    <FormControlLabel
+                        className="checkBoxName"
+                        value="???"
+                        control={<Checkbox color="secondary" />}
+                        label="Nail care"
+                        classes={{ root: classes.options }}
+                    />
+                </div>
+
+                <div className="itemBox">
+                    <FormControlLabel
+                        className="checkBoxName"
+                        value="???"
+                        control={<Checkbox color="secondary" />}
+                        label="Hair Extensions"
+                        classes={{ root: classes.options }}
+                    />
+                </div>
+
+                <div className="itemBox">
+                    <FormControlLabel
+                        className="checkBoxName"
+                        value="???"
+                        control={<Checkbox color="secondary" />}
+                        label="Waxing"
+                        classes={{ root: classes.options }}
+                    />
+                </div>
+                <div>
+                    <Button
+                        key={"Book an appointment"}
+                        variant="contained"
+                        color="primary"
+                        style={{width: "100%", }}
+                    >
+                        Book an appointment
+                    </Button>
+                </div>
             </div>
-
-            <div className="itemBox">
-                <FormControlLabel className="checkBoxName"
-                    value="???"
-                    control={<Checkbox color="secondary" className="checkBoxes"/>}
-                    label="Hair Care"
-                    labelPlacement="start"
-                />
-            </div>
-
-            <div className="itemBox">
-                <FormControlLabel className="checkBoxName"
-                    value="???"
-                    control={<Checkbox color="secondary" />}
-                    label="Haircuts"
-                    labelPlacement="start"
-                />
-            </div>
-
-            <div className="itemBox">
-                <FormControlLabel className="checkBoxName"
-                    value="???"
-                    control={<Checkbox color="secondary" />}
-                    label="Nail care"
-                    labelPlacement="start"
-                />
-            </div>
-
-            <div className="itemBox">
-                <FormControlLabel className="checkBoxName"
-                    value="???"
-                    control={<Checkbox color="secondary" />}
-                    label="Hair Extensions"
-                    labelPlacement="start"
-                />
-            </div>
-
-            <div className="itemBox">
-                <FormControlLabel className="checkBoxName"
-                    value="???"
-                    control={<Checkbox color="secondary" />}
-                    label="Waxing"
-                    labelPlacement="start"
-                />
-            </div>
-
-
         </div>
     );
-}
+};
 export default SideBar;
