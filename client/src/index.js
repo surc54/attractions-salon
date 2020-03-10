30a820c97182ac3f93dfab9d58a1db3ab319a57c
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-03-10 11:29:00 -0400

    Remove body overflow setting and fix html font size

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import "normalize.css/normalize.css";
+import "semantic-ui-css/semantic.min.css";
 import "./components/global.scss";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -7,7 +8,6 @@ import history from "./models/history";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import 'semantic-ui-css/semantic.min.css';
 
 const theme = createMuiTheme({
     palette: {
