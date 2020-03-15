a4d8d10cad43a4b8ce9c739fb1dfcdabe4d76988
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-03-14 22:02:52 -0400

    Add reminder for authentication checks for Admin route

--- server/routes/admin/index.routes.js
+++ server/routes/admin/index.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const accountsRouter = require("./account.routes");
 
+// TODO: Authentication check when using ADMIN API
+
 router.use("/account", accountsRouter);
 
 module.exports = router;
