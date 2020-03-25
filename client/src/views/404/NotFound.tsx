ba906034a72860d926be93d8d54efd5e56973317
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-03-25 11:23:00 -0400

    Add actions to buttons

--- client/src/views/404/NotFound.tsx
+++ client/src/views/404/NotFound.tsx
@@ -1,16 +1,29 @@
-import React from "react";
-import styles from "./NotFound.module.scss";
-import pic from "../../assets/undraw_lost_bqr2.svg";
 import {
-    Grid,
-    Container,
-    useMediaQuery,
-    useTheme,
     Button,
+    Container,
+    Grid,
     Icon,
     Typography,
+    useMediaQuery,
+    useTheme,
 } from "@material-ui/core";
 import clsx from "clsx";
+import React from "react";
+import pic from "../../assets/undraw_lost_bqr2.svg";
+import history from "../../models/history";
+import styles from "./NotFound.module.scss";
+
+const goBack = () => {
+    history.goBack();
+};
+
+const goHome = () => {
+    history.push("/");
+};
+
+const goToContactUs = () => {
+    history.push("/contact");
+};
 
 const NotFound: React.FC<NotFoundProps> = () => {
     const theme = useTheme();
@@ -35,21 +48,33 @@ const NotFound: React.FC<NotFoundProps> = () => {
                         <pre>HTTP 404 - NOT FOUND</pre>
                         <h1>This page doesn't exist.</h1>
                         <div className={styles.actions}>
-                            <Button color="primary" variant="outlined">
+                            <Button
+                                color="primary"
+                                variant="outlined"
+                                onClick={goBack}
+                            >
                                 <Icon>arrow_back</Icon>
                                 <span className="spacer"></span>
                                 <Typography variant="button">
                                     Go back
                                 </Typography>
                             </Button>
-                            <Button color="primary" variant="outlined">
+                            <Button
+                                color="primary"
+                                variant="outlined"
+                                onClick={goHome}
+                            >
                                 <Icon>home</Icon>
                                 <span className="spacer"></span>
                                 <Typography variant="button">
                                     Go Home
                                 </Typography>
                             </Button>
-                            <Button color="primary" variant="outlined">
+                            <Button
+                                color="primary"
+                                variant="outlined"
+                                onClick={goToContactUs}
+                            >
                                 <Icon>phone</Icon>
                                 <span className="spacer"></span>
                                 <Typography variant="button">
