c50dfd546f36177ad1e92e583907fe8d54fa4d75
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-03-27 12:23:13 -0400

    push into my branch = immediate death (cherry-pick)

---
+++ server/models/item.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const serviceItem = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, min: 0},
+    description: {type: String},
+    subtitle: {type: String},
+    imgURL: {type: String, default:
+        "https://www.lakegastonrentals.com/images/blog/Time-for-something-new-switch.jpg"    
+        // temporary, may have copyright
+    },
+
+});
+
+module.exports = mongoose.model('serviceItem', serviceItem);
