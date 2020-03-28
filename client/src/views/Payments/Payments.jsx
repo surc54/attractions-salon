67dbd3e42e28a80c5aa57aef811e01bddf4530f5
Adrian Salazar <adrian.salazar@ufl.edu>
2020-03-28 00:16:30 -0400

    Set up of stepper, styling and 3rd page next

--- client/src/views/Payments/Payments.jsx
+++ client/src/views/Payments/Payments.jsx
@@ -1,62 +1,106 @@
-import React from "react";
-import { makeStyles, Grid, Paper } from "@material-ui/core";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Stepper, Step, StepLabel, Button, Typography} from '@material-ui/core';
 import "./Payments.css";
-import RightWindow from "./RightWindow";
-import AppointmentWindow from "./AppointmentWindow";
-import QuickLinksWindow from "./QuickLinksWindow";
-import AmountDueWindow from "./AmountDueWindow";
+
+import FirstStep from "./FirstStep";
+import SecondStep from "./SecondStep";
 
 const useStyles = makeStyles(theme => ({
-    window: {
-        height: "100%",
-        marginTop: "7rem",
-        display: "flex",
-        justifyContent: "center",
+    root: {
+      width: '100%',
     },
-    empty: {
-        width: "auto", // just to have something here
-        height: "50rem",
+    backButton: {
+      marginRight: theme.spacing(1),
     },
-}));
+    instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+    },
+  }));
 
 const Payments = props => {
     const classes = useStyles();
     return (
         <div className="Payments">
-
-            <Grid container spacing={1} className={classes.window}>
-                <Grid item xs={12} md={4}>
-                    <Paper className={classes.empty} elevation={1}>
-                        <RightWindow />
-                    </Paper>
-                </Grid>
-            </Grid>
-
-            <Grid container spacing={2} justify="space-evenly" direction="row">
-                <Grid item xs={4}>
-                    <Paper className={classes.paper} elevation={1}>
-                        <AppointmentWindow />
-                    </Paper>
-                </Grid>
-                <Grid container alignItems="flex-end" spacing={2} justify="center">
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper} elevation={1}>
-                            <AmountDueWindow />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper} elevation={1}>
-                            <QuickLinksWindow />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-
-
-
+            <HorizontalLabelPositionBelowStepper/>
         </div>
     );
 };
 
 export default Payments;
+
+
+
+
+
+function getSteps() {
+  return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return <FirstStep/>;
+    case 1:
+      return <SecondStep/>;
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
+const HorizontalLabelPositionBelowStepper = () => {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map(label => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            {getStepContent(activeStep)}
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
