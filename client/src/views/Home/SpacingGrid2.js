75ddfd93d1a2032c741d7ce1d2065eb12bb1c6a0
Jessica O'Dell <odelljessica@ufl.edu>
2020-03-30 10:40:02 -0400

    added Slideshow and edits to grids

--- client/src/views/Home/SpacingGrid2.js
+++ client/src/views/Home/SpacingGrid2.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
+import styles from "./Home.module.scss";
 import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles(theme => ({
@@ -31,7 +27,8 @@ export default function SpacingGrid() {
         <Grid container justify="center" spacing={spacing}>
           {[0, 1, 2].map(value => (
             <Grid key={value} item>
-              <Paper/><div className = 'stylist-box'>Jane Doe</div>
+              <Paper/><div style = {{top: '-35%', fontSize: 25}}
+              className={styles["stylist-box"]}>Jane Doe</div>
             </Grid>
           ))}
         </Grid>
