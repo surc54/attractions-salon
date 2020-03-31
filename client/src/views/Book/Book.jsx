9aa0b10299d3ac1fd36dd3de6b814345c6626da4
Dylan Hawley <dylanthomashawley@gmail.com>
2020-03-31 07:35:08 -0400

    date and time pickers placed into form

--- client/src/views/Book/Book.jsx
+++ client/src/views/Book/Book.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { makeStyles, Paper } from "@material-ui/core";
-import Grid from '@material-ui/core/Grid';
+import { Grid, TextField, Typography } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import "./Book.css";
@@ -13,53 +12,57 @@ const Book = props => {
     };
     
     return (
-        <div className="bigDiv">
-
-<Grid container spacing={1}>
-            <Grid item xs={12} md={4}>
-                <Paper elevation={1}>
-                    <p>uwu</p>
-                </Paper>
-            </Grid>
-        </Grid>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-                <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="date-picker-inline"
-                label="Date picker inline"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                }}
-                />
-                <KeyboardDatePicker
-                margin="normal"
-                id="date-picker-dialog"
-                label="Date picker dialog"
-                format="MM/dd/yyyy"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                }}
-                />
-                <KeyboardTimePicker
-                margin="normal"
-                id="time-picker"
-                label="Time picker"
-                value={selectedDate}
-                onChange={handleDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change time',
-                }}
-                />
-            </Grid>
-        </MuiPickersUtilsProvider>
+        <div className="content">
+            <Typography color="primary" variant="h4" className="title">
+                Book an Appointment
+            </Typography>
+            <form noValidate>
+                <Grid container spacing={0}>
+                    <Grid item xs={6}>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Name"
+                        variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Phone"
+                        variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <Grid container justify="space-around">
+                                <KeyboardDatePicker
+                                margin="normal"
+                                id="date-picker-dialog"
+                                label="Date picker dialog"
+                                format="MM/dd/yyyy"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                                />
+                                <KeyboardTimePicker
+                                margin="normal"
+                                id="time-picker"
+                                label="Time picker"
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change time',
+                                }}
+                                />
+                            </Grid>
+                        </MuiPickersUtilsProvider>
+                    </Grid>
+                </Grid>
+            </form>
         </div>
     );
 };
