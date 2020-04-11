2e95f3ee70897412c68f2a001c98d0fa3564d83e
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-04-11 11:28:12 -0400

    Add backend of ez-settings

---
+++ server/controllers/ez-setting.controller.js
@@ -0,0 +1,82 @@
+const {
+    send_code_error,
+    send_code_success,
+    requiredBody,
+} = require("../tools");
+const EzSetting = require("../models/ez-setting.model");
+
+const KEY_REGEX = /^[A-Za-z\-_]+$/;
+
+module.exports.get = (req, res) => {
+    const { key } = req.query;
+
+    // make sure key exists
+    if (!key) {
+        send_code_error(res, 400, "ez-settings/get/missing-key");
+        return;
+    }
+
+    // make sure key is valid
+    if (!KEY_REGEX.test(key)) {
+        send_code_error(res, 400, "ez-settings/get/bad-key");
+        return;
+    }
+
+    // key is okay. check with database.
+    EzSetting.findOne({ key })
+        .then((resp) => {
+            // check if valid document.
+            if (!resp || !resp._id) {
+                send_code_error(res, 404, "ez-settings/get/not-found");
+                return;
+            }
+
+            // send document
+            send_code_success(res, 200, "ez-settings/get/success", {
+                data: resp.toObject({ versionKey: false }),
+            });
+        })
+        .catch((err) => {
+            // something went wrong.
+            send_code_error(res, 500, "ez-settings/get/error", { error: err });
+        });
+};
+
+module.exports.set = [
+    requiredBody(["key", "value"], undefined, "ez-settings/set/missing-%s"),
+    (req, res) => {
+        let { key, value } = req.body;
+
+        // no need to check if key/value actually exists, as requiredBody should've taken care of that
+
+        // make sure key is valid
+        if (!KEY_REGEX.test(key)) {
+            send_code_error(res, 400, "ez-settings/set/bad-key");
+            return;
+        }
+
+        // force value to string
+        if (typeof value !== "string") {
+            if (value.toString) {
+                value = value.toString();
+            } else {
+                value = String(value);
+            }
+        }
+
+        EzSetting.create({
+            key,
+            value,
+        })
+            .then((resp) => {
+                send_code_success(res, 200, "ez-settings/set/success", {
+                    data: { key, value },
+                });
+            })
+            .catch((err) => {
+                send_code_error(res, 500, "ez-settings/set/error", {
+                    error: err,
+                });
+            });
+    },
+];
--- server/routes/admin/index.routes.js
+++ server/routes/admin/index.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const accountsRouter = require("./account.routes");
+const ezSettingsRouter = require("./ez-setting.routes");
 
 // TODO: Authentication check when using ADMIN API
 
 router.use("/account", accountsRouter);
+router.use("/ez-setting", ezSettingsRouter);
 
 module.exports = router;
