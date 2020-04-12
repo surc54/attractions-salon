b2a56de4d9cce924d6b921843e0d66041217cc64
Adithya Haridas <34602379+surc54@users.noreply.github.com>
2020-04-12 12:35:35 -0400

    Add reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
 import _loginReducer, { UserState } from "./_loginReducer";
+import _ezSettingsReducer from "./_ezSettingsReducer";
 
 export default combineReducers<ReduxState>({
     login: _loginReducer,
+    ezSettings: _ezSettingsReducer as any,
 });
 
 export interface ReduxState {
     login: UserState;
+    ezSettings: {
+        [key: string]: {
+            loading?: boolean;
+            value?: string;
+            error?: any;
+        };
+    };
 }
